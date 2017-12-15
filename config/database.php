4d1141e6b9cf93572b6d8dde179a2af4591b38f1
Marco M. Vidal <marcomvidal@gmail.com>
2017-12-15 00:57:34 -0200

    Mudança de banco de dados para PostgreSQL

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,11 +68,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'ec2-23-23-245-89.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd6u9o7eta54apf'),
+            'username' => env('DB_USERNAME', 'nrkxchasxxxfmz'),
+            'password' => env('DB_PASSWORD', '2fc23fcecb5ebfcf15a33a71d47d0cd1f611ce016925250f8dbee3a6db70ffd7'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
